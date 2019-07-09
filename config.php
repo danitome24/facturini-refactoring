e9b574139f3414b8f9449576288ababb63e3d64d
Daniel Tome Fernandez <danieltomefer@gmail.com>
2019-07-09 19:36:24 +0200

    Added config file

---
+++ config.php
@@ -0,0 +1,11 @@
+<?php
+
+$dbhost = 'mysql';
+$dbuname = getenv('MYSQL_USER');
+$dbpass = getenv('MYSQL_PASSWORD');
+$dbname = getenv('MYSQL_DATABASE');
+$dbtype = "MySQL";
+$dbDebugMode = false;
+
+$table_name = "factura";
+
